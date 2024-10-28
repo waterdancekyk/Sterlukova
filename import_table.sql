ffac3bff1a6fe391c707f2a55fab97576b9a1588
waterdancekyk <iaros.rijkow2015@mail.ru>
2024-10-28 12:03:14 +0300

    qweqwejqw

--- import_table.sql
+++ import_table.sql
@@ -1,9 +1,7 @@
 #mode csv
-import 'partners.csv' Partners
-import 'sales_history.csv' SalesHistory
-import 'import_model.csv' import_model
-import 'item.csv' item_list
-import 'mangers.cvs' mangers
-import 'ratihb_history.csv' ratihb_history
-import 'requests.csv' requests
-import 
+import "item.csv" item
+import 'mangers.csv' mangers
+import 'pathers.csv' pathers
+import 'rating_history.csv' rating_history
+import 'requests.cvs' requests
+import 'sales_history.csv' sales_history
